bbe60179cbedd9b8f277322a4ebbe59e67cbbe25
zhuxiuwei <zhuxiuwei@meituan.com>
2023-09-27 18:08:55 +0800

    更新swiftserver 还是跑不起来

--- annotation-version/src/main/java/com/xiuwei/rpc_server/Server3.java
+++ annotation-version/src/main/java/com/xiuwei/rpc_server/Server3.java
@@ -1,14 +1,29 @@
 package com.xiuwei.rpc_server;
 
 import com.facebook.swift.codec.ThriftCodecManager;
+import com.facebook.swift.service.ThriftEventHandler;
+import com.facebook.swift.service.ThriftServer;
+import com.facebook.swift.service.ThriftServerConfig;
 import com.facebook.swift.service.ThriftServiceProcessor;
 import com.xiuwei.sevice.TestService;
 import com.xiuwei.sevice.TestServiceImpl;
 import org.apache.thrift.TProcessor;
 
+import java.util.ArrayList;
+
+/**
+ * 参考swift官方文档：https://github.com/facebookarchive/swift/tree/master/swift-service
+ * 还参考了示例代码：https://www.tabnine.com/code/java/classes/com.facebook.swift.service.ThriftServiceProcessor
+ * 可以运行了，但是和运行ServerRunner效果一样，客户端报一样的错：org.apache.thrift.transport.TTransportException: Could not find sequenceId in Thrift message
+ */
 public class Server3 {
     public static void main(String[] args) {
         TestService service = new TestServiceImpl();
-//        TProcessor processor = new ThriftServiceProcessor(new ThriftCodecManager(), );
+        ThriftServiceProcessor processor = new ThriftServiceProcessor(new ThriftCodecManager(),
+                new ArrayList<ThriftEventHandler>(), service);
+        ThriftServerConfig config = new ThriftServerConfig();
+        config.setPort(9090);
+        ThriftServer thriftServer = new ThriftServer(processor, config);
+        thriftServer.start();
     }
 }
--- annotation-version/src/main/java/com/xiuwei/rpc_server/ServerRunner.java
+++ annotation-version/src/main/java/com/xiuwei/rpc_server/ServerRunner.java
@@ -2,7 +2,28 @@ package com.xiuwei.rpc_server;
 
 import com.facebook.swift.service.ThriftServer;
 
+/**
+ * 参考：https://blog.csdn.net/weixin_33701294/article/details/90305200
+ * 跑不通，server可以运行，但是client运行会报错：
+ * org.apache.thrift.transport.TTransportException: Could not find sequenceId in Thrift message
+ * 	at com.facebook.nifty.client.AbstractClientChannel.extractSequenceId(AbstractClientChannel.java:102)
+ * 	at com.facebook.nifty.client.AbstractClientChannel.sendAsynchronousRequest(AbstractClientChannel.java:174)
+ * 	at com.facebook.swift.service.SyncClientHelpers.sendSynchronousTwoWayMessage(SyncClientHelpers.java:52)
+ * 	at com.facebook.swift.service.ThriftMethodHandler.synchronousInvoke(ThriftMethodHandler.java:164)
+ * 	at com.facebook.swift.service.ThriftMethodHandler.invoke(ThriftMethodHandler.java:131)
+ * 	at com.facebook.swift.service.ThriftClientManager$ThriftInvocationHandler.invoke(ThriftClientManager.java:520)
+ * 	at com.sun.proxy.$Proxy6.isOk(Unknown Source)
+ * 	at com.xiuwei.client.ThriftClientDemo.main(ThriftClientDemo.java:20)
+ * Caused by: java.lang.AbstractMethodError: org.apache.thrift.transport.TTransport.checkReadBytesAvailable(J)V
+ * 	at org.apache.thrift.protocol.TBinaryProtocol.checkStringReadLength(TBinaryProtocol.java:444)
+ * 	at org.apache.thrift.protocol.TBinaryProtocol.readStringBody(TBinaryProtocol.java:415)
+ * 	at org.apache.thrift.protocol.TBinaryProtocol.readString(TBinaryProtocol.java:411)
+ * 	at org.apache.thrift.protocol.TBinaryProtocol.readMessageBegin(TBinaryProtocol.java:251)
+ * 	at com.facebook.nifty.client.AbstractClientChannel.extractSequenceId(AbstractClientChannel.java:98)
+ * 	... 7 more
+ */
 public class ServerRunner {
+
     public static void main(String[] args) {
         ServerCreator serverCreator = new ServerCreator().invoke();
         ThriftServer server = serverCreator.getServer();
